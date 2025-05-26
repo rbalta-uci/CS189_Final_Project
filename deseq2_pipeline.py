0a4708e418d0c6e19bc18670826e7860bf172694
rbalta-uci <rbalta@uci.edu>
2025-05-26 14:01:35 -0700

    got deseq2 working with synthetic data

--- deseq2_pipeline.py
+++ deseq2_pipeline.py
@@ -23,3 +23,41 @@ DATA_PATH = "https://raw.githubusercontent.com/owkin/PyDESeq2/main/datasets/synt
 
 counts_df = pd.read_csv(os.path.join(DATA_PATH, "test_counts.csv"), index_col=0)
 print(counts_df)
+
+counts_df = counts_df.T
+print(counts_df)
+
+metadata = pd.read_csv(os.path.join(DATA_PATH, "test_metadata.csv"), index_col=0)
+print(metadata)
+
+samples_to_keep = ~metadata.condition.isna()
+counts_df = counts_df.loc[samples_to_keep]
+metadata = metadata.loc[samples_to_keep]
+
+genes_to_keep = counts_df.columns[counts_df.sum(axis=0) >= 10]
+counts_df = counts_df[genes_to_keep]
+
+inference = DefaultInference(n_cpus=8)
+dds = DeseqDataSet(
+    counts=counts_df,
+    metadata=metadata,
+    refit_cooks=True,
+    inference=inference,
+)
+
+dds.deseq2()
+
+print(dds)
+
+with open(os.path.join(OUTPUT_PATH, "result_dds.pkl"), "wb") as f:
+    pkl.dump(dds, f)
+    
+print(dds.varm["dispersions"])
+
+print(dds.varm["LFC"])
+
+ds = DeseqStats(dds, contrast=["condition", "B", "A"], inference=inference)
+
+ds.summary()
+
+ds.results_df.to_csv(os.path.join(OUTPUT_PATH, "results.csv"))
