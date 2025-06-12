15981af33e2088d906e9180aad7f8a0e6789023b
Rachel Balta <rachelhbalta@gmail.com>
2025-06-12 16:14:58 -0700

    final submission

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import scanpy as sc
+
 def preprocessing():
     ann_data = sc.datasets.ebi_expression_atlas("E-MTAB-9543")
 
@@ -9,13 +10,15 @@ def preprocessing():
     # Drop NA cell types
     ann_data = ann_data[~ann_data.obs["cell_type"].isna()].copy()
     sc.pp.filter_genes(ann_data, min_cells=10)
-    sc.pp.filter_cells(ann_data, min_counts=1000)  # optional
+    sc.pp.filter_cells(ann_data, min_counts=1000)
+
     # Convert to DataFrame
     counts_df = pd.DataFrame.sparse.from_spmatrix( ann_data.X.astype(np.float32), index=ann_data.obs_names, columns=ann_data.var_names)
     counts_df = counts_df.astype(np.int32)
+
     # Metadata
     metadata = ann_data.obs.copy()
-    metadata["condition"] = metadata["cell_type"]  # Add 'condition' column
+    metadata["condition"] = metadata["cell_type"] 
     return counts_df, metadata
 
 def relevant_genes(file, output):
@@ -32,4 +35,4 @@ def relevant_genes(file, output):
                     continue
 
 if __name__ == "__main__":
-    relevant_genes("./output_files/synthetic_example/results.csv", "./output_files/actual_results.csv")
+    relevant_genes("./output_files/results.csv", "./output_files/actual_results.csv")
--- test_deseq2_realdata.py
+++ test_deseq2_realdata.py
@@ -1,21 +1,14 @@
 import os
-import pickle as pkl
-import pandas as pd
 from pydeseq2.dds import DeseqDataSet
 from pydeseq2.default_inference import DefaultInference
 from pydeseq2.ds import DeseqStats
-import diffxpy.api as de
-import matplotlib.pylab as plt
-import numpy as np
-import scanpy as sc
 from preprocessing import preprocessing
+
 if __name__ == "__main__":
-    # Replace this with the path to directory where you would like results to be saved
-    OUTPUT_PATH = "./output_files/synthetic_example/"
-    os.makedirs(OUTPUT_PATH, exist_ok=True)  # Create path if it doesn't exist
+    OUTPUT_PATH = "./output_files/"
+    os.makedirs(OUTPUT_PATH, exist_ok=True) 
 
     counts_df, metadata = preprocessing()
-    # DESeq2 Setup
     inference = DefaultInference(n_cpus=8)
     dds = DeseqDataSet(
         counts=counts_df,
@@ -26,7 +19,6 @@ if __name__ == "__main__":
 
     dds.deseq2()
 
-    # Contrast setup (automatically pick top 2 conditions)
     conditions = metadata["condition"].value_counts().index[:2].tolist()
     print("Using contrast between:", conditions)
 
